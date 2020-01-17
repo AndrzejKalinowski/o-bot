d40d47098d3e76ed6bc4ba6411b762669c62314c
AndrzejKalinowski <56219781+AndrzejKalinowski@users.noreply.github.com>
2020-01-17 22:31:16 +0100

    Update main.cpp

--- o-bot_auto_test/src/main.cpp
+++ o-bot_auto_test/src/main.cpp
@@ -22,6 +22,7 @@ const byte medianFilterWindowSize = 5;
 const byte medianFilterWindowSize2 = 5;
 Servo myservo;  // tworzenie obiektu servo
 Servo myservo2; // tworzenie obiektu servo2
+Servo myservo3;
 SharpDistSensor sensor1(sharp1Pin, medianFilterWindowSize);
 SharpDistSensor sensor2(sharp2Pin, medianFilterWindowSize2);
 
@@ -31,6 +32,7 @@ int SpeedValue = 0;
 int iloszprzejazdow = 0;
 
 unsigned int distance;
+const unsigned int MAX_DIST = 23200;
 
 void turn_right();
 void turn_left();
@@ -44,7 +46,7 @@ void setup()
 {
   myservo.attach(2);  // podlaczenie serva do pinu 2
   myservo2.attach(3); // podlaczenie serva2 do pinu 3
-
+  myservo3.attach(4);
   for (int i; i < 9; i++)
   {
     pinMode(i, OUTPUT);
@@ -74,6 +76,8 @@ void setup()
 
   digitalWrite(7, LOW);
   digitalWrite(6, LOW);
+
+  myservo3.write(40);
   Serial.println("Ready");
 }
 
@@ -156,48 +160,13 @@ void loop()
   digitalWrite(13, HIGH);
   digitalWrite(8, LOW);
   digitalWrite(12, LOW);
-  if ((hcsr04Read(2) <= 1500) && (iloszprzejazdow < 2))
+  if ((hcsr04Read(2) <= 600) && (iloszprzejazdow < 2))
   {
     turn_right();
     iloszprzejazdow++;
-    if (iloszprzejazdow > 1)
-    {
-      delay(80);
-      digitalWrite(6, HIGH);
-      /*
-      if (measure.RangeMilliMeter <= 150)
-      {
-
-        digitalWrite(7, HIGH);
-
-        digitalWrite(11, HIGH);
-        digitalWrite(13, LOW);
-        digitalWrite(8, LOW);
-        digitalWrite(12, LOW);
-
-      }
-      else
-      {
-        digitalWrite(7, LOW);
-      }
-
-      if (measure.RangeMilliMeter >= 150)
-      {
-        digitalWrite(11, LOW);
-        digitalWrite(13, HIGH);
-        digitalWrite(8, LOW);
-        digitalWrite(12, LOW);
-      }
-
-    }
-    else
-    {
-      digitalWrite(6, LOW);
-    }
-    */
-    }
-
   }
+
+
   LeftMotor = 100;
   RightMotor = 100;
   analogWrite(9, RightMotor);
@@ -205,7 +174,7 @@ void loop()
   if (iloszprzejazdow < 2)
   {
 
-    if (measure.RangeMilliMeter <= 150)
+    if (measure.RangeMilliMeter <= 100)
     {
       /*
     LeftMotor += 1000;
@@ -241,7 +210,7 @@ void loop()
       digitalWrite(7, LOW);
     }
 
-    if (measure.RangeMilliMeter >= 150)
+    if (measure.RangeMilliMeter >= 100)
     {
       /*
     RightMotor += 50;
@@ -273,11 +242,13 @@ void loop()
       //analogWrite(9,  RightMotor);
       //analogWrite(10, LeftMotor);
     }
-    if ((hcsr04Read(2) <= 1500) && (iloszprzejazdow < 2))
+    /*
+    if ((hcsr04Read(2) <= 1700) && (iloszprzejazdow < 2))
     {
       turn_right();
       iloszprzejazdow++;
     }
+    */
   }
   else
   {
@@ -286,25 +257,138 @@ void loop()
     // digitalWrite(8, LOW);
     // digitalWrite(12, LOW);
   }
-  if ((hcsr04Read(2) <= 1500) && (iloszprzejazdow >= 2))
+  if (iloszprzejazdow == 2)
   {
+    Serial.println("ok");
+    while (measure.RangeMilliMeter <= 200)
+    {
+      VL53L0X_RangingMeasurementData_t measure;
+
+      //Serial.print("Reading a measurement... ");
+      lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+    //  if (measure.RangeStatus != 4)
+    if (measure.RangeStatus != 4)
+    { // phase failures have incorrect data
+         Serial.print("Distance (mm): ");
+       Serial.println(measure.RangeMilliMeter);
+    }
+    else
+    {
+         Serial.println(" out of range ");
+    }
+
+    if (measure.RangeMilliMeter <= 60)
+    {
+      digitalWrite(7, HIGH);
+
+      digitalWrite(11, HIGH);
+      digitalWrite(13, LOW);
+      digitalWrite(8, LOW);
+      digitalWrite(12, LOW);
+    }
+    else
+    {
+      digitalWrite(7, LOW);
+    }
+
+    if (measure.RangeMilliMeter >= 60)
+    {
+
+      digitalWrite(11, LOW);
+      digitalWrite(13, HIGH);
+      digitalWrite(8, LOW);
+      digitalWrite(12, LOW);
+
+    }
+    else
+    {
+    }
+    if (measure.RangeMilliMeter >= 150) {
+      delay(50);
+      iloszprzejazdow ++;
+      break;
+    }
+    }
+    digitalWrite(11, HIGH);
+    digitalWrite(13, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(12, LOW);
+  }
+
+  if ((hcsr04Read(2) <=600) && (iloszprzejazdow >= 2) && (hcsr04Read(2) > 200))
+  {
+
+
     turn_left();
     iloszprzejazdow++;
   }
+  if(iloszprzejazdow >= 3){
+    if (hcsr04Read(1) >= 700)
+    {
+      digitalWrite(7, HIGH);
 
+      digitalWrite(11, HIGH);
+      digitalWrite(13, LOW);
+      digitalWrite(8, LOW);
+      digitalWrite(12, LOW);
+    }
+    else
+    {
+      digitalWrite(7, LOW);
+    }
+
+    if (hcsr04Read(1) <= 700)
+    {
+
+
+      digitalWrite(11, LOW);
+      digitalWrite(13, HIGH);
+      digitalWrite(8, LOW);
+      digitalWrite(12, LOW);
+
+    }
+    else
+    {
+    }
+  }
   // forward();
   //Serial.print("sharp: ");
   //Serial.println(sharpRead(2));
-  Serial.println(iloszprzejazdow);
+  //
+  //VL53L0X_RangingMeasurementData_t measure;
+
+  //Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+//  if (measure.RangeStatus != 4)
+if (measure.RangeStatus != 4)
+{ // phase failures have incorrect data
+  //   Serial.print("Distance (mm): ");
+   //Serial.println(measure.RangeMilliMeter);
+}
+else
+{
+  //   Serial.println(" out of range ");
+}
+  //Serial.println(hcsr04Read(1));
   //delay(10);
-  //Serial.println(hcsr04Read(2));
+//  mStop();
+  //while(1){
+  Serial.println(hcsr04Read(2));
+//}
   //forward();
   //turn_left();
   //delay(500);
 
 
 
+if (iloszprzejazdow == 5) {
+  //while(1){
+  //  mStop();
 
+  //}
+}
 //end loop
 
 }
@@ -317,7 +401,7 @@ void turn_right()
   digitalWrite(12, HIGH);
   analogWrite(9, 150);
   analogWrite(10, 150);
-  delay(450);
+  delay(460);
   analogWrite(10, 0);
   analogWrite(9, 0);
 }
@@ -331,7 +415,7 @@ void turn_left()
   digitalWrite(12, LOW);
   analogWrite(9, 150);
   analogWrite(10, 150);
-  delay(420);
+  delay(395);
   analogWrite(10, 0);
   analogWrite(9, 0);
 }
@@ -360,21 +444,58 @@ int hcsr04Read(int a)
   switch (a)
   {
   case 1:
+    /*
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
     return pulseIn(echoPin, HIGH);
+    */
+    unsigned long t1;
+    unsigned long t2;
+    unsigned long pulse_width;
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    while ( digitalRead(echoPin) == 0 );
+    t1 = micros();
+    while ( digitalRead(echoPin) == 1);
+    t2 = micros();
+    pulse_width = t2 - t1;
+    if ( pulse_width > MAX_DIST ) {
+      Serial.println("Out of range");
+    } else {
+      return pulse_width;
+    }
+
     break;
 
   case 2:
+    /*
     digitalWrite(trigPin2, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin2, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin2, LOW);
     return pulseIn(echoPin2, HIGH);
+    */
+    unsigned long t1;
+    unsigned long t2;
+    unsigned long pulse_width;
+    digitalWrite(trigPin2, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin2, LOW);
+    while ( digitalRead(echoPin2) == 0 );
+    t1 = micros();
+    while ( digitalRead(echoPin2) == 1);
+    t2 = micros();
+    pulse_width = t2 - t1;
+    if ( pulse_width > MAX_DIST ) {
+      Serial.println("Out of range");
+    } else {
+      return pulse_width;
+    }
     break;
 
   default:
@@ -410,3 +531,4 @@ void errorXD()
   while (1)
     ;
 }
+// the end!
